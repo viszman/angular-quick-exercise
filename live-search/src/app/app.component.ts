d1cd43d4322464eb7bb2ad8432602ced28621fdd
Daniel Klimowicz <viszman@gmail.com>
2019-11-26 08:26:15 +0100

    simple live search

--- live-search/src/app/app.component.ts
+++ live-search/src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import * as data from './source';
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'live-search';
+  media: ListItem[] = data.data;
+
+  liveSearch = new Subject();
+
+  initMedia: ListItem[] = [...data.data];
+  filter = [
+    'Videos',
+    'Interactive Video',
+    'Audio',
+    'Images',
+    'Documents'
+  ];
+
+  selectedFilter: string;
+  sortDir: 'ASC' | 'DESC' = 'ASC';
+
+  clear() {
+    this.sortDir = 'ASC';
+    this.media = [...this.initMedia];
+  }
+
+  filterByType(filterOption: string) {
+    this.selectedFilter = filterOption;
+
+    this.media = this.initMedia.filter((item) => {
+      return item.type === filterOption;
+    })
+  }
+
+  filterByKeyword(keyword: string) {
+    this.media = this.media.filter(item => {
+      return item.title.includes(keyword) === true;
+    })
+  }
+
+  sortMedia(dir: 'ASC' | 'DESC') {
+    this.media.sort(function (itemA: ListItem, itemB: ListItem) {
+      const date1 = new Date(itemA.created);
+      const date2 = new Date(itemB.created);
+      if (dir === 'ASC') {
+        return date1.getTime() - date2.getTime();
+      } else {
+        return date2.getTime() - date1.getTime();
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.liveSearch.subscribe((keyword:string) => {
+      this.filterByKeyword(keyword);
+    })
+  }
+
+  changeSortDir() {
+    if (this.sortDir === 'ASC') {
+      this.sortDir = 'DESC';
+
+    } else {
+      this.sortDir = 'ASC';
+    }
+    this.sortMedia(this.sortDir);
+  }
+}
+
+export interface ListItem {
+  title: string;
+  type: string;
+  created: string
 }
---
+++ live-search/src/app/source.ts
@@ -0,0 +1,73 @@
+export const data = [
+  {
+    "title": "asdasd2323 Video",
+    "type": "Videos",
+    "created": "2001-09-09 17:11:02"
+  },
+
+  {
+    "title": "Li2322m Video",
+    "type": "Videos",
+    "created": "2003-09-09 17:11:02"
+  },
+  {
+    "title": "2342352dfsdf item Video",
+    "type": "Videos",
+    "created": "2004-09-09 17:11:02"
+  },
+  {
+    "title": "asdf asf Video",
+    "type": "Videos",
+    "created": "2006-09-09 17:11:02"
+  },
+  {
+    "title": "Som love dfaadf Documents",
+    "type": "Documents",
+    "created": "2007-09-09 17:11:02"
+  },
+  {
+    "title": "List item Interactive",
+    "type": "Interactive Video",
+    "created": "2004-09-09 17:11:02"
+  },
+  {
+    "title": "List item Video",
+    "type": "Videos",
+    "created": "2002-09-09 17:11:02"
+  },
+  {
+    "title": "List item Video",
+    "type": "Videos",
+    "created": "2009-09-09 17:11:02"
+  },
+  {
+    "title": "List item Interactive",
+    "type": "Interactive Video",
+    "created": "2005-09-09 17:11:02"
+  },
+  {
+    "title": "List item Video",
+    "type": "Videos",
+    "created": "2003-12-09 17:11:02"
+  },
+  {
+    "title": "List item Audio",
+    "type": "Audio",
+    "created": "2002-07-09 17:11:02"
+  },
+  {
+    "title": "List item Video",
+    "type": "Videos",
+    "created": "2001-01-09 17:11:02"
+  },
+  {
+    "title": "List item Audio",
+    "type": "Audio",
+    "created": "2001-04-01 17:11:02"
+  },
+  {
+    "title": "List item Images",
+    "type": "Images",
+    "created": "2017-02-09 17:11:02"
+  }
+];
